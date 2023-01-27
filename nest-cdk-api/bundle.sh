27a915672845ba0f71ba2dcc1296fea8bdae4923
devsimiyu <dev.wrodgerss@gmail.com>
2023-01-27 22:43:07 +0300

    cache node dependencies on /tmp inside docker bundling container without permissions errors

--- nest-cdk-api/bundle.sh
+++ nest-cdk-api/bundle.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo 'INSTALL DEPENDENCIES AND SAVE CACHE IN "./.npm"'
+echo 'INSTALL DEPENDENCIES'
 npm install
 
 echo 'RUN BUILD'
@@ -11,6 +11,3 @@ npm run build $1
 echo 'COPY ARTIFACTS FOR DISTRIBUTION'
 cp -r ./node_modules /asset-output/node_modules
 cp -r ./dist /asset-output/dist
-
-echo 'CLEANUP NPM CACHE'
-rm -rf ./.npm
